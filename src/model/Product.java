f59c425609602749acc08f8962e7479c51d68e0f
prashant-sth25 <shresthaprashant25@gmail.com>
2025-07-08 21:27:03 +0545

    Product page

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,28 @@
 package src.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+    private String location;
+
+    public Product(int id, String name, int quantity, String location) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.location = location;
+    }
+
+    //adding getter and setter methods
+    public int getId(){return id;}
+    public String getName(){return name;}
+    public int getQuantity(){return quantity;}
+    public String getLocation(){return location;}
+
+    public void setQuantity(int quantity){this .quantity = quantity;}
     
+    @Override
+    public String toString(){
+        return "ID: " + id + ", Name: " + name + ", Quantity: " + quantity + ", Location: " + location;
+    }
 }
